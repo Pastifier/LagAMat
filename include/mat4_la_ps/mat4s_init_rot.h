52e366fde3b5727bf6efd2a8a3b558b9dde14a24
Pastifier <136130975+Pastifier@users.noreply.github.com>
2024-09-21 23:31:25 +0400

    Update mat4s_init_rot.h

--- include/mat4_la_ps/mat4s_init_rot.h
+++ include/mat4_la_ps/mat4s_init_rot.h
@@ -20,39 +20,40 @@
 /// @param rad Rotation in radians
 static inline t_mat4s	lag_mat4s_rotation_x(float rad)
 {
-	return ((t_mat4s)
-		{
-			.r1.simd = _mm_set_ps(0.f, 0.f, 0.f, 1.f),
-			.r2.simd = _mm_set_ps(0.f, -sin(rad), cos(rad), 0.f),
-			.r3.simd = _mm_set_ps(0.f, cos(rad), sin(rad), 0.f),
-			.r4.simd = _mm_set_ps(1.f, 0.f, 0.f, 0.f)
-		});
+	t_mat4s	ret;
+
+	ret.r1.simd = _mm_set_ps(0.f, 0.f, 0.f, 1.f);
+	ret.r2.simd = _mm_set_ps(0.f, -sin(rad), cos(rad), 0.f);
+	ret.r3.simd = _mm_set_ps(0.f, cos(rad), sin(rad), 0.f);
+	ret.r4.simd = _mm_set_ps(1.f, 0.f, 0.f, 0.f);
+	return (ret);
 }
 
 /// @brief Returns a rotation matrix around the y-axis.
 /// @param rad Rotation in radians
 static inline t_mat4s	lag_mat4s_rotation_y(float rad)
 {
-	return ((t_mat4s)
-		{
-			.r1.simd = _mm_set_ps(0.f, sin(rad), 0.f, cos(rad)),
-			.r2.simd = _mm_set_ps(0.f, 0.f, 1.f, 0.f),
-			.r3.simd = _mm_set_ps(0.f, cos(rad), 0.f, -sin(rad)),
-			.r4.simd = _mm_set_ps(1.f, 0.f, 0.f, 0.f)
-		});
+	t_mat4s	ret;
+
+
+	ret.r1.simd = _mm_set_ps(0.f, sin(rad), 0.f, cos(rad));
+	ret.r2.simd = _mm_set_ps(0.f, 0.f, 1.f, 0.f);
+	ret.r3.simd = _mm_set_ps(0.f, cos(rad), 0.f, -sin(rad));
+	ret.r4.simd = _mm_set_ps(1.f, 0.f, 0.f, 0.f);
+	return (ret);
 }
 
 /// @brief Returns a rotation matrix around the z-axis.
 /// @param rad Rotation in radians
 static inline t_mat4s	lag_mat4s_rotation_z(float rad)
 {
-	return ((t_mat4s)
-		{
-			.r1.simd = _mm_set_ps(0.f, 0.f, -sin(rad), cos(rad)),
-			.r2.simd = _mm_set_ps(0.f, 0.f, cos(rad), sin(rad)),
-			.r3.simd = _mm_set_ps(0.f, 1.f, 0.f, 0.f),
-			.r4.simd = _mm_set_ps(1.f, 0.f, 0.f, 0.f)
-		});
+	t_mat4s	ret;
+	
+	ret.r1.simd = _mm_set_ps(0.f, 0.f, -sin(rad), cos(rad));
+	ret.r2.simd = _mm_set_ps(0.f, 0.f, cos(rad), sin(rad));
+	ret.r3.simd = _mm_set_ps(0.f, 1.f, 0.f, 0.f);
+	ret.r4.simd = _mm_set_ps(1.f, 0.f, 0.f, 0.f);
+	return (ret);
 }
 
 #endif // !MAT4S_INIT_ROT_H
