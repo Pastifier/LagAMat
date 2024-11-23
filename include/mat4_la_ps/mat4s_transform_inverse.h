46b240c5852de34a2953cdecd1a7aeb86b3b3db5
Emran BinJamaan <136130975+Pastifier@users.noreply.github.com>
2024-11-23 07:12:35 +0400

    Update mat4s_transform_inverse.h

--- include/mat4_la_ps/mat4s_transform_inverse.h
+++ include/mat4_la_ps/mat4s_transform_inverse.h
@@ -15,6 +15,47 @@
 
 # include "ltypes.h"
 
+/*Process documented in https://docs.google.com/document/d/1ok8dzMk7EZiZaVRB61zGDlxRSDoelX3z6ixaCRlg0yM/edit?usp=sharing*/
+
+/// @brief Calculates the inverse of a decomposed matrix
+/// (rotation, scale, translation)
+/// @param rot The rotational component of the matrix
+/// @param s The scale factor of the matrix
+/// @param t The translation component of the matrix
+/// @return The inverse of the decomposed matrix
+/// @warning The scale should NOT contain any zero components as that will
+/// cause divide-by-zero errors! (And it doesn't really make sense anyway)
+static inline t_mat4s	lag_mat4s_get_transform_inverse(const t_mat4s rot, const __m128 s,
+			const __m128 t)
+{
+	t_mat4s			ret0;
+	__m128			tmp0, tmp1, tmp2, tmp3;
+	const __m128	rcps = _mm_div_ps(_mm_set1_ps(1.f), s);
+	const __m128	tinv = _mm_sub_ps(_mm_set1_ps(0.f), t);
+
+	tmp0 = _mm_unpacklo_ps(rot.simd[0], rot.simd[1]);
+	tmp1 = _mm_unpackhi_ps(rot.simd[0], rot.simd[1]);
+	tmp2 = _mm_unpacklo_ps(rot.simd[2], rot.simd[3]);
+	tmp3 = _mm_unpackhi_ps(rot.simd[2], rot.simd[3]);
+
+	ret0.simd[0] = _mm_movelh_ps(tmp0, tmp2);
+	ret0.simd[1] = _mm_movehl_ps(tmp2, tmp0);
+	ret0.simd[2] = _mm_movelh_ps(tmp1, tmp3);
+	ret0.simd[3] = _mm_set_ps(1, 0, 0, 0);
+
+	ret0.simd[0] = _mm_mul_ps(ret0.simd[0],
+			_mm_shuffle_ps(rcps, rcps, _MM_SHUFFLE(0, 0, 0, 0)));
+	ret0.simd[1] = _mm_mul_ps(ret0.simd[1],
+			_mm_shuffle_ps(rcps, rcps, _MM_SHUFFLE(1, 1, 1, 1)));
+	ret0.simd[2] = _mm_mul_ps(ret0.simd[2],
+			_mm_shuffle_ps(rcps, rcps, _MM_SHUFFLE(2, 2, 2, 2)));
+
+	ret0.r1.w = _mm_cvtss_f32(_mm_dp_ps(ret0.simd[0], tinv, 0xF1));
+	ret0.r2.w = _mm_cvtss_f32(_mm_dp_ps(ret0.simd[1], tinv, 0xF1));
+	ret0.r3.w = _mm_cvtss_f32(_mm_dp_ps(ret0.simd[2], tinv, 0xF1));
+	return (ret0);
+}
+
 //static inline t_mat4s lag_mat4s_get_transform_inverse(const t_mat4s s,
 //						const t_mat4s r, const t_mat4s t)
 //{
@@ -74,40 +115,5 @@
 
 //    return (s1rr);
 //}
-
-static inline t_mat4s lag_mat4s_get_transform_inverse(const t_mat4s rot, const __m128 s, const __m128 t)
-{
-	t_mat4s ret0;
-	t_vec4s trns;
-	__m128  tmp0, tmp1, tmp2, tmp3;
-
-	const __m128 rcps = _mm_div_ps(_mm_set1_ps(1.f), s);
-	const __m128 tinv = _mm_sub_ps(_mm_set1_ps(0.f), t);
-
-	ret0.simd[0] = _mm_mul_ps(rot.simd[0], rcps);
-	ret0.simd[1] = _mm_mul_ps(rot.simd[1], rcps);
-	ret0.simd[2] = _mm_mul_ps(rot.simd[2], rcps);
-
-	ret0.simd[0] = _mm_mul_ps(ret0.simd[0], _mm_shuffle_ps(tinv, tinv, _MM_SHUFFLE(0, 0, 0, 0)));
-	ret0.simd[1] = _mm_mul_ps(ret0.simd[1], _mm_shuffle_ps(tinv, tinv, _MM_SHUFFLE(1, 1, 1, 1)));
-	ret0.simd[2] = _mm_mul_ps(ret0.simd[2], _mm_shuffle_ps(tinv, tinv, _MM_SHUFFLE(2, 2, 2, 2)));
-
-	trns.simd = _mm_add_ps(_mm_add_ps(ret0.simd[0], ret0.simd[1]), ret0.simd[2]);
-	
-	tmp0 = _mm_unpacklo_ps(rot.simd[0], rot.simd[1]); // [r00, r10, r01, r11]
-	tmp1 = _mm_unpackhi_ps(rot.simd[0], rot.simd[1]); // [r02, r12, r03, r13]
-	tmp2 = _mm_unpacklo_ps(rot.simd[2], rot.simd[3]); // [r20, r30, r21, r31]
-	tmp3 = _mm_unpackhi_ps(rot.simd[2], rot.simd[3]); // [r22, r32, r23, r33]
-
-	ret0.simd[0] = _mm_movelh_ps(tmp0, tmp2);
-	ret0.simd[1] = _mm_movehl_ps(tmp2, tmp0);
-	ret0.simd[2] = _mm_movelh_ps(tmp1, tmp3);
-	ret0.simd[3] = _mm_set_ps(1, 0, 0, 0);
-
-	ret0.r1.w = trns.x;
-	ret0.r2.w = trns.y;
-	ret0.r3.w = trns.z;
-	return (ret0);
-}
 
 #endif // !MAT4S_TRANSFORM_INVERSE
